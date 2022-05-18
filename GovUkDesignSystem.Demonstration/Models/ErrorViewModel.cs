1e4a20c0d6ebbaa59bcf1ce52e089898ab902f44
Dan Corder <dan.corder@softwire.com>
2022-05-18 18:16:55 +0100

    feat: Add website project

---
+++ GovUkDesignSystem.Demonstration/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace GovUkDesignSystem.Demonstration.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
