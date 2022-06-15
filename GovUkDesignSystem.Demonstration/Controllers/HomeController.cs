fe1664399d267afea206a3a5e03c3852e3b86cf4
Dan Corder <dan.corder@softwire.com>
2022-06-15 19:55:52 +0100

    feat: Add button demo page

--- GovUkDesignSystem.Demonstration/Controllers/HomeController.cs
+++ GovUkDesignSystem.Demonstration/Controllers/HomeController.cs
@@ -35,6 +35,12 @@ public class HomeController : Controller
     {
         return View();
     }
+    
+    [HttpGet]
+    public IActionResult Button()
+    {
+        return View();
+    }
 
     [HttpGet]
     public IActionResult TextInput()
